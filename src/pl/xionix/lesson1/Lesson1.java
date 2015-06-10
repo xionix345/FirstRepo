7ef84f391ca26013aea8dade3037b6e8fbdb8778
Xionix <piotmar892@student.polsl.pl>
2015-06-10 19:14:35 +0200

    First commit

---
+++ src/pl/xionix/lesson1/Lesson1.java
@@ -0,0 +1,11 @@
+package pl.xionix.lesson1;
+
+public class Lesson1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello wordl! Let's start Java!!");
+	}
+
+}
